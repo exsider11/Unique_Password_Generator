64d9e953353c4ad8108abca65244ffbe3abf92f6
exsider11 <exsider1231@gmail.com>
2023-10-23 17:22:06 -0400

    edited readme

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # 03 JavaScript: Password Generator
 
-##Project asks users to set parameters when creating a password.
-##Then creates a randomly generated password using given parameters.
-##After, project displays the newly generated password in an alert box and into the box inside the webpage.
+## Project asks users to set parameters when creating a password.
+## Then creates a randomly generated password using given parameters.
+## After, project displays the newly generated password in an alert box and into the box inside the webpage.
--- script.js
+++ script.js
@@ -19,6 +19,7 @@ function generatePassword(passwordLength,upper,lower,numeric,special){
     chars = chars + specials;
   }
   var passwordLength;
+  
   var password = "";
   for (var i = 0; i <= passwordLength; i++) {
     var randomNumber = Math.floor(Math.random() * chars.length);
@@ -29,18 +30,20 @@ function generatePassword(passwordLength,upper,lower,numeric,special){
 // Write password to the #password input
 function writePassword() {
   var passwordID = document.getElementById("password");
-  passwordID.readOnly = false;
+
   var length,upper,lower,numeric,special;
   length = prompt("how long should the password be?");
   upper = confirm("should the password have uppercases?");
   lower = confirm("should the password have lower cases?");
   numeric = confirm("should the password include numbers?");
   special = confirm("should the password include special characers?");
+
   if(length == false || upper == false || lower == false || numeric == false || special == false){
     alert("you dont want anything for your password?")
     alert("try again")
     return;
   }
+
   var password = generatePassword(length,upper,lower,numeric,special);
   alert(password);
   passwordID.innerHTML = password;
